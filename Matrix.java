c5137c150ed46148bfe87312fd219c3f513353bd
tltnsk <147326194+tltnsk@users.noreply.github.com>
2025-01-10 23:25:05 +0200

    Add files via upload

---
+++ Matrix.java
@@ -0,0 +1,25 @@
+package ana.finki.aud01;
+
+public class Matrix {
+    public static double sum(double[][] matrix) {
+        double sum = 0;
+
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[i].length; j++){
+                sum += matrix[i][j];
+            }
+        }
+        return sum;
+    }
+
+    public static double average(double[][] matrix) {
+        return sum(matrix)/ (matrix.length * matrix[0].length);
+    }
+
+    public static void main(String[] args) {
+        double[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}};
+
+        System.out.println(sum(matrix));
+        System.out.println(average(matrix));
+    }
+}
