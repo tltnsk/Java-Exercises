ac9b247e998c888978037bf05c812bf430935e85
tltnsk <147326194+tltnsk@users.noreply.github.com>
2025-01-13 19:37:23 +0200

    Add files via upload

---
+++ Lab05/IntegerArray.java
@@ -0,0 +1,42 @@
+package problem1;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class IntegerArray extends ResizableArray<Integer>{
+    public IntegerArray() {
+        super();
+    }
+
+    private IntStream getStream() {
+        return Arrays.stream(toArray()).limit(count()).mapToInt(Integer.class::cast);
+    }
+
+    public double sum() {
+        return getStream().sum();
+    }
+
+    public double mean(){
+        return sum() / count();
+    }
+
+    public int countNonZero() {
+        return (int) getStream().filter(i -> i != 0).count();
+    }
+
+    public IntegerArray distinct() {
+        IntegerArray ia = new IntegerArray();
+
+        getStream().distinct().forEach(ia::addElement);
+
+        return ia;
+    }
+
+    public IntegerArray increment(int offset) {
+        IntegerArray ia = new IntegerArray();
+
+        getStream().map(i -> i + offset).forEach(ia::addElement);
+
+        return ia;
+    }
+}
